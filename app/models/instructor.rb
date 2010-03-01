3f95b1e52ac13878ae9493e6eeab3e852d5d4944
belmont <belmont@cf3ce388-ae38-4ca7-8541-a07f109aa5c6>
2010-03-01 22:39:58 +0000

    added instructors controller to the default visible controllers and created the show for the instructors. Cannot currently edit instructors.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,5 +1,8 @@
 class Instructor < ActiveRecord::Base
   has_many :works
   has_many :syllabus_files
+  has_many :courses, :through => :works, :uniq => true
+  has_many :projects, :through => :works, :uniq => true
   
+  # , :uniq => true
 end
