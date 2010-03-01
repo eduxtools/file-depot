3e14522f833962192e0bd828c5995947452dd479
James Pierce <jpegjames@users.noreply.github.com>
2010-03-01 22:39:58 +0000

    added instructors controller to the default visible controllers and created the show for the instructors. Cannot currently edit instructors.

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -3,4 +3,7 @@ class InstructorsController < ApplicationController
     @instructors = Instructor.find(:all, :order => 'name ASC',  :conditions => ['name LIKE ?', "%#{params[:search]}%"])
   end
 
+  def show
+    @instructor = Instructor.find(params[:id]) # Eager loading must not be used, as a rails bug(?) does not allow :uniq to work with eager loading 
+  end
 end
