3f6dac4dbf464468a7ade02a8969d9c8badc469a
James Pierce <jpegjames@users.noreply.github.com>
2010-04-07 02:05:43 +0000

    added two new helpers: image_height() and image_width(). These two helpers will find the height and width of thumbnail images based off a parent id. Usage: image_height(parent_id, thumbnail_name) #=> numerical value

--- app/helpers/browse/works_helper.rb
+++ app/helpers/browse/works_helper.rb
@@ -1,2 +1,11 @@
 module Browse::WorksHelper
+  
+  def image_height(parent_id, thumbnail_name)
+    Image.find_by_parent_id(parent_id, :conditions => { :thumbnail => thumbnail_name } ).height
+  end
+  
+  def image_width(parent_id, thumbnail_name)
+    Image.find_by_parent_id(parent_id, :conditions => { :thumbnail => thumbnail_name } ).width
+  end
+  
 end
