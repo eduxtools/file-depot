6795b1ba5fbe230839b9a2f48122684cf96db310
James Pierce <james@jpegjames.com>
2015-10-16 12:47:59 -0500

    define default includes for as_json

--- app/models/syllabus.rb
+++ app/models/syllabus.rb
@@ -4,4 +4,11 @@ class Syllabus < ActiveRecord::Base
 
   has_attached_file :document
   validates_attachment_content_type :document, content_type: ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
+
+  def as_json(options={})
+    default_include = {:instructor => {:only => :name}, :course => {:only => :name}}
+    options[:include] = options[:include] ? default_include.merge(options[:include]) : default_include 
+
+    super(options)
+  end
 end
