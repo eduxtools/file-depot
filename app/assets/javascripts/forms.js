60b03e27d87ff9655b28d01833f950194feb5e33
James Pierce <james@jpegjames.com>
2015-12-30 10:15:58 -0600

    improved form functionality

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,7 +1,52 @@
 $(document).ready(function() {
-  $(".chosen").chosen({
-    disable_search_threshold: 10,
-    allow_single_deselect: true
-  });
+  init_chosen('body');
 })
   
+function init_chosen(selector) {
+  // init chosen
+  $(selector + ' .chosen').chosen({
+    disable_search_threshold: 10,
+    allow_single_deselect: true,
+    // max_selected_options: console.log(this)
+  });
+
+  // setup allow new feature
+  $(selector + ' select[data-allow-new]').each(function() {
+    setup_allow_new_chosen($(this), $($(this).data('chosen').container[0]))
+  })
+}
+
+function setup_allow_new_chosen(original_select, chosen_select) {
+  chosen_select.parent().find('.chosen-container .search-field input[type=text]').keydown( 
+      function (evt) {
+         var stroke, _ref, target, list;
+         // get keycode
+         stroke = (_ref = evt.which) != null ? _ref : evt.keyCode;
+         // If enter or tab key
+         if (stroke === 9 || stroke === 13) {
+             target = $(evt.target);               
+             // get the list of current options
+             chosenList = target.parents('.chosen-container').find('.chosen-choices li.search-choice > span').map(function () { return $(this).text(); }).get();
+             // get the list of matches from the existing drop-down
+             matchList = target.parents('.chosen-container').find('.chosen-results li').map(function () { return $(this).text(); }).get();
+             // highlighted option
+             highlightedList = target.parents('.chosen-container').find('.chosen-results li.highlighted').map(function () { return $(this).text(); }).get();
+             // Get the value which the user has typed in
+             var newString = $.trim(target.val());
+             // if the option does not exists, and the text doesn't exactly match an existing option, and there is not an option highlighted in the list
+             if ($.inArray(newString,matchList) < 0 && $.inArray(newString,chosenList) < 0 && highlightedList.length == 0) {
+               // Create a new option and add it to the list (but don't make it selected)
+               var newOption = '<option value="::new_record::' + newString + '">' + newString + '</option>';
+               original_select.prepend(newOption);
+               // trigger the update event
+               original_select.trigger("chosen:updated");
+               // tell chosen to close the list box
+               original_select.trigger("chosen:close");
+               return true;
+            } 
+            // otherwise, just let the event bubble up
+            return true;
+         }
+      }
+  )
+}
--- app/helpers/input_helper.rb
+++ app/helpers/input_helper.rb
@@ -1,54 +1,97 @@
 module InputHelper
 
-  def text_field_with_bar(form_object, method, options = {})
-    content_tag :div, class: "input-wrapper #{options[:wrapper_class]}", id: options[:wrapper_id], 'data-overlay-label' => options[:overlay_label] do 
-      if options[:label] == true
-        label_html = form_object.label(method, class: "#{'required' if options[:required] == true}")
-      elsif options[:label]
-        label_html = form_object.label(method, options[:label].html_safe, class: "#{'required' if options[:required] == true}") 
-      else
-        label_html = ''.html_safe
-      end
+  def build_label_for_bar(f, method, options={})
+    label = options[:label]
 
-      label_html +
-      form_object.text_field(method, options) + 
-      content_tag(:span, '', class: 'bar')
+    if label == true
+      f.label method
+    elsif label == :match
+      f.label method, '&nbsp;'.html_safe, class: 'hide-for-small-only'
+    elsif label.is_a?(String)
+      f.label method, label.html_safe, class: "#{'required' if options[:required] == true}"
+    else
+      # no label
+      ''
+    end
+  end
+
+  def build_label_tag_for_bar(method, options={})
+    label = options[:label]
+
+    if label == true
+      label_tag method
+     elsif label == :match
+      label_tag method, '&nbsp;'.html_safe, class: 'hide-for-small-only'
+    elsif label.is_a?(String)
+      label_tag method, label.html_safe, class: "#{'required' if options[:required] == true}"
+    else
+      # no label
+      ''
+    end
+  end
+
+  def build_bar
+    content_tag :span, nil, class: 'bar'
+  end
+
+  def text_field_with_bar(form_object, method, options = {})
+    content_tag :div, class: "input-wrapper #{options[:wrapper_class]}", id: options[:wrapper_id], 'data-overlay-label' => options[:overlay_label] do
+      concat build_label_for_bar(form_object, method, options)
+      concat form_object.text_field(method, options)
+      concat build_bar
+    end
+  end
+
+  def text_field_tag_with_bar(method, value, options = {})
+    content_tag :div, class: "input-wrapper #{options[:wrapper_class]}", id: options[:wrapper_id], 'data-overlay-label' => options[:overlay_label] do
+      concat build_label_tag_for_bar(method, options)
+      concat text_field_tag(method, value, options)
+      concat build_bar
+    end
+  end
+
+  def password_field_tag_with_bar(method, value, options = {})
+    content_tag :div, class: "input-wrapper #{options[:wrapper_class]}", id: options[:wrapper_id], 'data-overlay-label' => options[:overlay_label] do
+      concat build_label_tag_for_bar(method, options)
+      concat password_field_tag(method, value, options)
+      concat build_bar
     end
   end
 
   def text_area_with_bar(form_object, method, options = {})
     options = {rows: 7}.merge(options) # default row count is 7
 
-    content_tag :div, class: "input-wrapper #{options[:wrapper_class]}", id: options[:wrapper_id], 'data-overlay-label' => options[:overlay_label] do 
-      if options[:label] == true
-        label_html = form_object.label(method)
-      elsif options[:label]
-        label_html = form_object.label(method, options[:label].html_safe) 
-      else
-        label_html = ''.html_safe
-      end
-
-      label_html +
-      form_object.text_area(method, options) + 
-      content_tag(:span, '', class: 'bar')
+    content_tag :div, class: "input-wrapper #{options[:wrapper_class]}", id: options[:wrapper_id], 'data-overlay-label' => options[:overlay_label] do
+      concat build_label_for_bar(form_object, method, options)
+      concat form_object.text_area(method, options)
+      concat build_bar
     end
   end
 
-  # this isn't dry because it is similar to text_field_with_bar
+  def text_area_tag_with_bar(method, value, options = {})
+    options = {rows: 7}.merge(options) # default row count is 7
+
+    content_tag :div, class: "input-wrapper #{options[:wrapper_class]}", id: options[:wrapper_id], 'data-overlay-label' => options[:overlay_label] do
+      concat build_label_tag_for_bar(method, options)
+      concat text_area_tag(method, value, options)
+      concat build_bar
+    end
+  end
+
   def chosen_select(form_object, method, choices=nil, options={}, html_options={})
-    content_tag :div, class: "input-wrapper chosen-wrapper #{options[:wrapper_class]}", id: options[:wrapper_id], 'data-overlay-label' => options[:overlay_label] do 
-      if options[:label] == true
-        label_html = form_object.label(method)
-      elsif options[:label]
-        label_html = form_object.label(method, options[:label].html_safe) 
-      else
-        label_html = ''.html_safe
-      end
-      html_options[:class] = 'chosen ' + html_options[:class].to_s
+    html_options = {class: 'chosen'}.merge(html_options)
+    html_options[:class] = 'chosen ' + html_options[:class].to_s
+    html_options[:id] = html_options[:id] if html_options[:id]
 
-      label_html +
-      form_object.select(method, choices, options, html_options) + 
-      content_tag(:span, '', class: 'bar')
+    content_tag :div, class: "input-wrapper chosen-wrapper #{options[:wrapper_class]}", id: options[:wrapper_id], 'data-overlay-label' => options[:overlay_label] do
+      concat build_label_for_bar(form_object, method, options)
+      concat form_object.select(method, choices, options, html_options)
+      concat build_bar
     end
   end
+
+  def chosen_select_currency(form_object, method, options={}, html_options={})
+    chosen_select(form_object, method, options_for_select(AppConfig['currency_options'], form_object.object.send(method)), options, html_options)
+  end
+
 end
