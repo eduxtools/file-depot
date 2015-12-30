650fe4cfe4e1bc195b82243d360197c0de2db50d
James Pierce <james@jpegjames.com>
2015-12-30 10:16:42 -0600

    defined application start page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   resources :students
   resources :works
   resources :attachments, only: [:create, :destroy]
+
+  root 'application#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
