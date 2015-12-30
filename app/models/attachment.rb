fb58e7c0738a80712eef011eee3d2fe86bcee2f9
James Pierce <james@jpegjames.com>
2015-12-30 14:19:49 -0600

    do not validate :file (for now) and `whiny` set to false

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,14 +1,15 @@
 class Attachment < ActiveRecord::Base
   belongs_to :parent, polymorphic: true
 
-  has_attached_file :file, default_url: "/images/:style/missing.png", 
+  has_attached_file :file, whiny: false, default_url: "/images/:style/missing.png", 
     styles: { 
       large: "1200x900>",
       medium: "950x700>", 
       small: "300x300>", 
       thumb: "100x100>" 
     }
-  validates_attachment_content_type :file, content_type: /\Aimage\/.*\Z/
+  do_not_validate_attachment_file_type :file
+  # validates_attachment_content_type :file, content_type: /\*/
 
   def temp_id=(id)
     write_attribute(:parent_id, id)
