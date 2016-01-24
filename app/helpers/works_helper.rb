e0e82bc8790daca06f1627595a242e7be102870c
James Pierce <james@jpegjames.com>
2016-01-24 14:33:50 -0600

    added 'image' icon

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -1,30 +1,34 @@
 module WorksHelper
-  def file_icon(content_type)
+  def file_icon(content_type, html_options={})
     case content_type
 	  when "application/x-shockwave-flash"
-	    image_tag('icons/Adobe_Flash.png')
+	    image_tag('icons/Adobe_Flash.png', html_options)
 	  when "application/postscript"
-	    image_tag('icons/Adobe_Illustrator.png')
+	    image_tag('icons/Adobe_Illustrator.png', html_options)
 	  when "application/pdf"
-	    image_tag('icons/File_Pdf.png')
+	    image_tag('icons/File_Pdf.png', html_options)
 	  when "application/msword"
-	    image_tag('icons/Word.png')
+	    image_tag('icons/Word.png', html_options)
 	  when "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-	    image_tag('icons/Word.png')
+	    image_tag('icons/Word.png', html_options)
 		when "text/php"
-	    image_tag('icons/Adobe_Dreamweaver.png')
+	    image_tag('icons/Adobe_Dreamweaver.png', html_options)
     # when "text/html"
-    #   image_tag('icons/Adobe_Dreamweaver.png')
+    #   image_tag('icons/Adobe_Dreamweaver.png', html_options)
     when "application/octet-stream" # seems to be attached to Word documents that were uploaded to Blackboard
-      image_tag('icons/News.png')
+      image_tag('icons/News.png', html_options)
 		when /^text/
-	    image_tag('icons/News.png')
+	    image_tag('icons/News.png', html_options)
 		when /^audio/
-	    image_tag('icons/Audio.png')
+	    image_tag('icons/Audio.png', html_options)
 		when /^video/
-	    image_tag('icons/Video.png')
+	    image_tag('icons/Video.png', html_options)
 	  else
-	    image_tag('icons/File.png')
+      if content_type.to_s.include? 'image'
+        image_tag('icons/Image.png', html_options)
+      else
+  	    image_tag('icons/File.png', html_options)
+      end
 	  end
   end
 end
