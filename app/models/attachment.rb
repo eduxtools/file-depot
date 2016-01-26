3cd0afeb726cb29a70a06e9c6b2c5cf54ef64d4f
James Pierce <james@jpegjames.com>
2016-01-25 23:11:33 -0600

    process non-web formats

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,12 +2,13 @@ class Attachment < ActiveRecord::Base
   belongs_to :parent, polymorphic: true
 
   has_attached_file :file, whiny: false, default_url: "/images/:style/missing.png", 
-    styles: { 
-      large: "1200x900>",
-      medium: "950x700>", 
-      small: "300x300>", 
-      thumb: "100x100>" 
-    }
+                    styles: lambda { |file| {
+                                large:  file.instance.format_for_web("1200x900>"),
+                                medium: file.instance.format_for_web("950x700>"),
+                                small:  file.instance.format_for_web("300x300>"),
+                                thumb:  file.instance.format_for_web("100x100>"),
+                              }
+                            }
   do_not_validate_attachment_file_type :file
   # validates_attachment_content_type :file, content_type: /\*/
 
@@ -15,4 +16,33 @@ class Attachment < ActiveRecord::Base
     write_attribute(:parent_id, id)
     write_attribute(:parent_type, 'temp')    
   end
+
+  def is_image?
+    [
+      'image/jpg',
+      'image/jpeg',
+      'image/pjpeg',
+      'image/png', 
+      'image/x-png', 
+      'image/gif',
+      'image/svg+xml',
+      'image/tiff',
+      'image/pict',
+      'image/vnd.microsoft.icon',
+      'image/vnd.adobe.photoshop',
+      'application/x-photoshop',
+      'application/postscript',
+      'application/pdf'
+    ].include?(self.file_content_type) 
+  end
+
+  def format_for_web(geometry)
+    web_formats           = ['.jpg', '.jpeg', '.png', '.gif']
+    options               = {}
+    options[:geometry]    = geometry
+    options[:format]      = :png if !web_formats.include?(File.extname(self.file_file_name))
+    return options
+  end
+
+
 end
