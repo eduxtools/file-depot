9d0af9eea63f1b697024a5f79fdf9c777c9b904b
James Pierce <james@jpegjames.com>
2016-01-26 00:52:46 -0600

    WIP sidebar for work details

--- app/controllers/browse/works_controller.rb
+++ app/controllers/browse/works_controller.rb
@@ -1,6 +1,7 @@
 class Browse::WorksController < ApplicationController
   layout 'browse'
   before_filter :load_works
+  before_filter :set_use_remote
   
   def index
   end
@@ -10,17 +11,27 @@ class Browse::WorksController < ApplicationController
   end
 
   private
+    def set_use_remote
+      @use_remote = true
+    end
+
     def load_works
+      if params[:project]
+        params[:course] = Project.find(params[:project]).try(:course).try(:id)
+      end
+
+      if params[:course]
+        params[:level] = Course.find(params[:course]).try(:level)
+      end
+
       conditions = {}
       conditions.merge!({instructor_id: params[:instructor]})                  unless params[:instructor].blank?
+      conditions.merge!({project_id: params[:project]})                        unless params[:project].blank?
       conditions.merge!({course_id: params[:course]})                          unless params[:course].blank?
       conditions.merge!({course_id: Course.where(level: params[:level]).ids})  unless params[:level].blank?
 
       unless conditions.blank?
         @works = Work.where(conditions)
       end
-
-      # temp
-      @works ||= Work.all
     end
 end
