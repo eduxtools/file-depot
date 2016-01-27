f33b6e68545177631e16d4f7f5d210b9ef5650a7
James Pierce <james@jpegjames.com>
2016-01-27 07:49:09 -0600

    save caches on save & destroy for work attachments

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -12,6 +12,9 @@ class Attachment < ActiveRecord::Base
   do_not_validate_attachment_file_type :file
   # validates_attachment_content_type :file, content_type: /\*/
 
+  after_save :update_work_caches
+  before_destroy :update_work_caches_after_delete
+
   def temp_id=(id)
     write_attribute(:parent_id, id)
     write_attribute(:parent_type, 'temp')    
@@ -43,6 +46,24 @@ class Attachment < ActiveRecord::Base
     options[:format]      = :png if !web_formats.include?(File.extname(self.file_file_name))
     return options
   end
+
+  private
+    def update_work_caches
+      if parent_type != 'temp' && parent && parent.class == Work 
+        parent.has_attachments = true
+        parent.has_images      = true if self.is_image?
+        parent.save
+      end
+    end
+
+    def update_work_caches_after_delete
+      if parent_type != 'temp' && parent && parent.class == Work 
+        other_attachments      = (parent.attachments - self)
+        parent.has_attachments = other_attachments.any?
+        parent.has_images      = other_attachments.any? && other_attachments.map(&:is_image?).include?(true)
+        parent.save
+      end
+    end
 
 
 end
