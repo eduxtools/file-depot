30a88809a1d006f854b0a9863c9ca93ffedc5ef5
James Pierce <james@jpegjames.com>
2016-01-27 14:42:26 -0600

    updated ui for which images/icons to show and when

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -1,34 +1,37 @@
 module WorksHelper
   def file_icon(content_type, html_options={})
-    case content_type
-	  when "application/x-shockwave-flash"
-	    image_tag('icons/Adobe_Flash.png', html_options)
-	  when "application/postscript"
-	    image_tag('icons/Adobe_Illustrator.png', html_options)
-	  when "application/pdf"
-	    image_tag('icons/File_Pdf.png', html_options)
-	  when "application/msword"
-	    image_tag('icons/Word.png', html_options)
-	  when "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-	    image_tag('icons/Word.png', html_options)
-		when "text/php"
-	    image_tag('icons/Adobe_Dreamweaver.png', html_options)
-    # when "text/html"
-    #   image_tag('icons/Adobe_Dreamweaver.png', html_options)
-    when "application/octet-stream" # seems to be attached to Word documents that were uploaded to Blackboard
-      image_tag('icons/News.png', html_options)
-		when /^text/
-	    image_tag('icons/News.png', html_options)
-		when /^audio/
-	    image_tag('icons/Audio.png', html_options)
-		when /^video/
-	    image_tag('icons/Video.png', html_options)
-	  else
-      if content_type.to_s.include? 'image'
-        image_tag('icons/Image.png', html_options)
-      else
-  	    image_tag('icons/File.png', html_options)
-      end
-	  end
+    image_tag(file_icon_path(content_type), html_options)
   end
+
+  def file_icon_path(content_type)
+    case content_type
+    when "application/x-shockwave-flash"
+      'icons/Adobe_Flash.png'
+    when "application/postscript"
+      'icons/Adobe_Illustrator.png'
+    when "application/pdf"
+      'icons/File_Pdf.png'
+    when "application/msword"
+      'icons/Word.png'
+    when "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      'icons/Word.png'
+    when "text/php"
+      'icons/Adobe_Dreamweaver.png'
+    # when "text/html"
+    #   'icons/Adobe_Dreamweaver.png'
+    when "application/octet-stream" # seems to be attached to Word documents that were uploaded to Blackboard
+      'icons/News.png'
+    when /^text/
+      'icons/News.png'
+    when /^audio/
+      'icons/Audio.png'
+    when /^video/
+      'icons/Video.png'
+    when '/^image/'
+      'icons/Image.png'
+    else
+      'icons/File.png'
+    end
+  end
+
 end
