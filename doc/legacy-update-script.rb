cb0d21b6df60c7f558c1749d1fa91a36ebfb482d
James Pierce <james@jpegjames.com>
2017-07-26 04:33:37 -0500

    legacy (v1) script for migrating data

--- doc/legacy-update-script.rb
+++ doc/legacy-update-script.rb
@@ -3,19 +3,41 @@
 # legacy models (using attachment_fu) to paperclip
 # -------------------------------------------------
 
-# Move images to attachments
-Legacy::Image.where(parent_id: nil).each do |image|
-  work = image.work
-  if work
-    attachment = work.attachments.create(file: File.new("#{Rails.root}/public#{image.public_filename}", 'r'))
+# remaining to process overnight: 1880 – 3900
+
+directory     = Rails.root
+directory     = '/Volumes/ServerRaid/WebServer/apps/nasad/'
+first_work_id = 1
+
+def process_images(first_id, last_id=first_id+100)
+  count         = 0
+  directory     = '/Volumes/ServerRaid/WebServer/apps/nasad/'
+  
+  # Move images to attachments
+  Legacy::Image.where(parent_id: nil, work_id: [first_id..last_id]).each do |image|
+    work = image.work
+    if work
+      file = File.open("#{directory}/public#{image.public_filename}", 'r')
+      attachment = work.attachments.create(file: file)
+      file.close
+
+      count += 1
+    end
   end
+
+  return "#{count} images processed"
 end
+
+
+
+
+
 
 # Move other_files to attachments
 Legacy::OtherFile.where(parent_id: nil).each do |other_file|
   work = other_file.work
   if work
-    attachment = work.attachments.create(file: File.new("#{Rails.root}/public#{other_file.public_filename}", 'r'))
+    attachment = work.attachments.create(file: File.new("#{directory}/public#{other_file.public_filename}", 'r'))
   end
 end
 
@@ -27,5 +49,5 @@ Legacy::SyllabusFile.each do |legacy_syllabus|
   syllabus.term           = legacy_syllabus.term
   syllabus.instructor_id  = legacy_syllabus.instructor_id
   syllabus.course_id      = legacy_syllabus.course_id
-  syllabus.document       = File.new("#{Rails.root}/public#{legacy_syllabus.public_filename}", 'r')
+  syllabus.document       = File.new("#{directory}/public#{legacy_syllabus.public_filename}", 'r')
 end
